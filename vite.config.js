bf786f7d40289d48ed763981d71058b04a12debd
TarasMysiura <118620052+TarasMysiura@users.noreply.github.com>
2023-02-19 20:53:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
+// import { defineConfig } from 'vite';
+// import { resolve } from 'path';
+// import injectHTML from 'vite-plugin-html-inject';
+// import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig({
-  root: 'src',
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'),
-        second: resolve(__dirname, 'src/restaurant-page.html'),
-        next: resolve(__dirname, 'src/hotel-page.html'),
-      },
-    },
-    outDir: '../dist',
-  },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-});
+// export default defineConfig({
+//   root: 'src',
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, 'src/index.html'),
+//         second: resolve(__dirname, 'src/restaurant-page.html'),
+//         next: resolve(__dirname, 'src/hotel-page.html'),
+//       },
+//     },
+//     outDir: '../dist',
+//   },
+//   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+// });
